b80323eb4503023054214c29291198a273f77dfc
Cathy Rolfs <crolfs47@gmail.com>
2022-09-27 15:17:51 -0500

    add code for caesar cipher method

---
+++ caesar-cipher.rb
@@ -0,0 +1,19 @@
+# Implement a caesar cipher that takes in a string and a shift factor and then outputs the modified string
+
+def caesar_cipher(string, shift)
+    new_string = ''
+    string.each_char do |char|
+        if char.ord.between?(65, 90)
+            num = ((char.ord - 65 + shift) % 26) + 65
+            new_string += num.chr
+        elsif char.ord.between?(97, 122)
+            num = ((char.ord - 97 + shift) % 26) + 97
+            new_string += num.chr
+        else new_string += char
+        end
+    end
+    new_string
+    p new_string
+end
+
+caesar_cipher("What a string!", 5)
