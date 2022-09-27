df336e157c719c692ed0810b7e5fcba0d1fff2bb
Cathy Rolfs <crolfs47@gmail.com>
2022-09-27 16:15:00 -0500

    refactor code to return a hash

--- sub-strings.rb
+++ sub-strings.rb
@@ -4,16 +4,23 @@
 # and how many times it was found.
 
 def substrings(word, dictionary)
+    hash = {}
     i = 1
     p dictionary
     while i <= word.length do
          if dictionary.include?(word[0,i])
+            if hash[word[0,i]] == nil
+                hash[word[0,i]] = 1
+            else
+                hash[word[0,i]] += 1
+            end
             p word[0,i]
          else
             p "does not include"
          end
         i +=1
     end
+    p hash
 end
 
 # word = "below"
