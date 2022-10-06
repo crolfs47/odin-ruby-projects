a26f9f1e2c367b5fdc9dd2af5b9d898b5ca4ac48
Cathy Rolfs <crolfs47@gmail.com>
2022-10-06 12:52:24 -0500

    set up game board

---
+++ tic-tac-toe.rb
@@ -0,0 +1,13 @@
+def display_board
+    row1 = " 1 | 2 | 3 "
+    row2 = " 4 | 5 | 6 "
+    row3 = " 7 | 8 | 9 "
+    divider = "---+---+---"
+    p row1
+    p divider
+    p row2
+    p divider
+    p row3
+end
+
+display_board
