23f550a8e1d5fa0c4a56fd82fc9b73a9de090fe4
Cathy Rolfs <crolfs47@gmail.com>
2022-11-08 11:34:25 -0600

    Format code

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,22 +1,22 @@
-# Build a method #bubble_sort that takes an array and returns a sorted array. 
+# Build a method #bubble_sort that takes an array and returns a sorted array.
 # It must use the bubble sort methodology (don't use #sort).
 
 def bubble_sort(array)
-    p array
-    sorted = false
-    while !sorted
-        i = 0
-        sorted = true
-        while i < array.length - 1
-            if array[i] > array[i+1]
-                array[i], array[i+1] = array[i+1], array[i]
-                sorted = false
-            end
-            i += 1
-        end
+  p array
+  sorted = false
+  until sorted
+    i = 0
+    sorted = true
+    while i < array.length - 1
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        sorted = false
+      end
+      i += 1
     end
-    p array
+  end
+  p array
 end
 
-array = [4,3,78,2,0,2]
+array = [4, 3, 78, 2, 0, 2]
 bubble_sort(array)
--- sub-strings.rb
+++ sub-strings.rb
@@ -1,16 +1,14 @@
-# Implement a method #substrings that takes a word as the first argument and then an 
+# Implement a method #substrings that takes a word as the first argument and then an
 # array of valid substrings (your dicttionary) as the second argument. It should return
 # a hash listing each substring (case insensitive) that was found in the original string
 # and how many times it was found.
 
 def substrings(string, dictionary)
-    hash = {}
-    dictionary.each do |word|
-        unless string.downcase.scan(word).length == 0
-            hash[word] = string.downcase.scan(word).length
-        end
-    end
-    p hash
+  hash = {}
+  dictionary.each do |word|
+    hash[word] = string.downcase.scan(word).length unless string.downcase.scan(word).length == 0
+  end
+  p hash
 end
 
 # string = "below"
@@ -19,7 +17,7 @@ end
 # string = "testing"
 # dictionary = ["test", "it", "sting", "test", "x"]
 
-string =  "Howdy partner, sit down! How's it going?"
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+string = "Howdy partner, sit down! How's it going?"
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
 substrings(string, dictionary)
